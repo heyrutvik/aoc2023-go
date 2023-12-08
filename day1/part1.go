2b35d91ff73f420a178f214082809a9babc19d75
Rutvik Patel <heyrutvik@gmail.com>
2023-12-08 16:41:00 +0530

    puzzle day1 part1: impl was missing

--- day1/part1.go
+++ day1/part1.go
@@ -1,5 +1,11 @@
 package day1
 
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 type Part1 struct{}
 
 func MakePart1() *Part1 {
@@ -7,9 +13,17 @@ func MakePart1() *Part1 {
 }
 
 func (p *Part1) Clean(line string) string {
-	panic("part1: not implemented")
+	var sb strings.Builder
+	for _, c := range line {
+		if unicode.IsDigit(c) {
+			sb.WriteRune(c)
+		}
+	}
+	return sb.String()
 }
 
 func (p *Part1) Calibrate(line string) (val int, err error) {
-	panic("part1: not implemented")
+	last := len(line) - 1
+	val, err = strconv.Atoi(string([]byte{line[0], line[last]}))
+	return
 }
